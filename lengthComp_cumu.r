9f85c7faad7ed8606c253fc9cffa066ab101aafe
jimrichardson00 <jimrichardson00@gmail.com>
2014-10-21 15:24:05 -0700

    put code for df_to_dat file in it's own r file, and replaced this code with line that sources code ( source() )

--- lengthComp_cumu.r
+++ lengthComp_cumu.r
@@ -10,47 +10,8 @@ scal_lengths <- lisread("scal_lengths.dat")
 # will be the same, except will add nObsBins, nstartBins, nendBins, and will change each length prop table
 scal_lengths_mod <- scal_lengths
 
-# data.frame to .dat file function
-df_to_dat <- function(data){
-	
-	# start with empty string
-	char <- ""
-
-	# for each row, spit out a character with all row elements separated by spaces
-	ncols <- length(as.data.frame(data))
-	nrows <- nrow(as.data.frame(data))
-
-	# if string is single element, convert it to character
-	if(ncols == 1 & nrows == 1){
-
-		char <- paste(as.character(data),"\n",sep="")
-
-	# if string is vector, cycle through elements of vector and output a single line
-	} else if (ncols == 1) {
-
-		char_row <- ""
-		for(j in seq(from=1,to=nrows,by=1)){
-			char_row <- paste(char_row,data[j]," ",sep="")
-		}
-		char <- paste(char_row,"\n",sep="")
-
-	# if string is data frame, cycle through rows and columns and output a line for each row
-	} else if (ncols > 1 & nrows > 1) {
-
-		data <- as.data.frame(data)
-
-		for(i in seq(from=1,to=nrows,by=1)){
-			char_row <- ""
-			for(j in seq(from=1,to=ncols,by=1)){
-				char_row <- paste(char_row,data[i,j]," ",sep="")
-			}
-			char <- paste(char,char_row,"\n",sep="")
-		}
-	}
-
-	return(char)
-}
-
+# sources df_to_dat.r, needed for df_to_dat function
+source("df_to_dat.r")
 
 # sets theshhold for which values are considered too small
 # will sum up values approaching from the top and bottom until threshhold is reached
