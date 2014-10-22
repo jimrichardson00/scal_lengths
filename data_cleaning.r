2bbe75a2616a146e33fd1b1e0f40ba5be1d619b5
jimrichardson00 <jimrichardson00@gmail.com>
2014-10-22 13:11:18 -0700

    added some comments

--- data_cleaning.r
+++ data_cleaning.r
@@ -1,9 +1,15 @@
 # data cleaning code
 
-# replaced RV_4VWX column in Number(gender)Measured.txt, with RV_4VWX column in Number(gender)Measured_copy.txt
-source("replace_RVWX_col.r")
+# sets working directory
+setwd("/home/jim/Dropbox/REM/tasks/scal_lengths")
 
-# take the propotion at length data (lengthComps_m etc.) and weigh it by the number measured data (NumberMaleMeasured etc.) for the commercial fisheries (RV and HS are unchanged) and output this into a dat file, scal_lengths.data
+# replaced RV_4VWX column in Number(gender)Measured.txt, with RV_4VWX column in Number(gender)Measured_copy.txt
+source("replace_RV_4VWX_col.r")
+
+# take the propotion at length data (lengthComps_m etc.) and weigh it by the number measured data (NumberMaleMeasured etc.) for the commercial fisheries (RV and HS are unchanged) and output this into a dat file, scal_lengths2.dat
+# outputs to scal_lenghts2.dat
+# calculates the difference from scal_lengths.dat for each prop table using matrix norm
+# all zero (or very small)
 source("makeLengthComps.r")
 
 # take scal_lengths.dat and for each year and fishery, aggregate the top and bottom length classes so they are at least 1%
