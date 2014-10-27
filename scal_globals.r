d7f5d957b901471722463305168e1b7c61062a70
jimrichardson00 <jimrichardson00@gmail.com>
2014-10-27 13:04:07 -0700

    initial commit, copied over from sean's files, needed for plotLenFit in scal_plots.r

---
+++ scal_globals.r
@@ -0,0 +1,33 @@
+# Global Options.
+# General globals.
+.INITYEAR <- 1970
+.LASTYEAR <- 2013
+.USEYEAR <- TRUE
+
+# General global variables.
+.CEXANNO      <- 1.0            # Annotation character expansion.
+.CEXANNO2     <- 1.2
+.CEXAXIS      <- 1.0            # Axis character expansion.
+.CEXAXIS2     <- 1.2
+.CEXLAB       <- 1.            # Label character expansion.
+.CEXLAB2      <- 1.4
+.CEXLEG       <- 1.0            # Legend character expansion.
+.CEXLEG2      <- 1.2
+.CEXSYM       <- 1.0
+.CEXSYM2      <- 1.4
+.ESTCOL       <- "black"        # Color of estimated values.
+.ESTLWD       <- 1              # Line width of estimated values.
+.INLINE1      <- 2
+.INLINE2      <- 3
+.INLINE3      <- 3
+.MAR          <- c(2,2,1,1)
+.OMA          <- c(4,7,1,1)
+.OUTLINE1     <- 1
+.OUTLINE2     <- 1
+.OUTLINE3     <- 0.5
+.OUTLINE4     <- 1.5
+.TRUECOL      <- "red3"         # Color of true values.
+.TRUELWD      <- 2              # Line width of true values.
+.YAXISLAS     <- 2              # y-Axis label orientation (1=vertical, 2=horiz)
+.LTYGROUP1    <- 1
+.LTYGROUP2    <- 2
