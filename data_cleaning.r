660559a395b68e47f7d90e7af4957df83f218371
jimrichardson00 <jimrichardson00@gmail.com>
2014-11-13 18:07:16 -0800

    sourced three seperate files, depending on how commerical fisheries are aggregated

--- data_cleaning.r
+++ data_cleaning.r
@@ -8,10 +8,16 @@ source("replace_RV_4VWX_col.r")
 
 # take the propotion at length data (lengthComps_m etc.) and weigh it by the number measured data (NumberMaleMeasured etc.) for the commercial fisheries (RV and HS are unchanged) and output this into a dat file, scal_lengths2.dat
 # outputs to scal_lenghts2.dat
-# calculates the difference from scal_lengths.dat for each prop table using matrix norm
-# all zero (or very small)
-source("makeLengthComps.r")
 
-# take scal_lengths2.dat and for each year and fishery, aggregate the top and bottom length classes so they are at least 1%
-# output the modified file as: scal_lengths_mod.dat
+# cf1
+source("makeLengthComps_cf1.r")
+
+# cf2
+source("makeLengthComps_cf2.r")
+
+# cf4
+source("makeLengthComps_cf4.r")
+
+# take output from makeLengthComps data and for each year and fishery, aggregate the top and bottom length classes so they are at least 1%
+# output the modified file in each case as: scal_lengths_cf_mod.dat
 source("lengthComp_cumu.r")
