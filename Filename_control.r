0b4bfa23df04fc919839528bd895a1368738f768
Jim Richardson <jimrichardson00>
2014-12-01 12:14:48 -0800

    inital commit, file to set the filenames of the input data

---
+++ Filename_control.r
@@ -0,0 +1,12 @@
+# sample data files
+samples_m <- "NumberMaleMeasured.txt"
+samples_f <- "NumberFemaleMeasured.txt" 
+samples_c <- "NumberCombinedMeasured.txt"
+
+# catch data
+landCatchMatrix <- "scaHalCatch_Index_lb5_ Sept152014 .dat.txt"
+
+# length composition data
+lengthComps_m <- "lengthComps_m.txt"
+lengthComps_f <- "lengthComps_f.txt"
+lengthComps_c <- "lengthComps_c.txt"
